21ff5bb02c1dc3119ee3627006ee9fcc491d1f55
acarnilsu <nilsuacar42@gmail.com>
2022-12-08 20:53:35 +0300

    Add project files.

---
+++ Api_Demo_Project/DAL/Context/CategoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Api_Demo_Project.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_Demo_Project.DAL.Context
+{
+    public class CategoryContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-O6Q5UAT;database=ApiDb; User Id=sa;Password=1234;");
+        }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
